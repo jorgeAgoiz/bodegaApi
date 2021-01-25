a355bcd7db6cfe984f4c15421cfb503fb363a2fe
jorgeAP <jorgeagoiz@gmail.com>
2021-01-25 16:34:10 +0100

    Update cerves.js

--- src/controllers/cerves.js
+++ src/controllers/cerves.js
@@ -87,7 +87,7 @@ exports.updateCerve = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ message: errors });
-  }
+  } // Necesitamos plantear el tema de actualizar y borrar fotos. ******************************************
   const { id } = req.query;
   try {
     let images = [];
